94bc1afa83a5cebe58efdba6551de627ea439646
FeuFeve <clement-potin@outlook.com>
2022-01-03 23:01:08 +0100

    Create voxelmaterial.h

---
+++ src/voxelmaterial.h
@@ -0,0 +1,11 @@
+#ifndef VOXELMATERIAL_H
+#define VOXELMATERIAL_H
+
+
+const unsigned char AIR = 0;
+const unsigned char STONE = 1;
+const unsigned char DIRT = 2;
+const unsigned char GRASS = 3;
+
+
+#endif // VOXELMATERIAL_H
