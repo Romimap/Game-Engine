1c7a006574ce6cea97acd05ea2b3184a8175e689
FeuFeve <clement-potin@outlook.com>
2022-01-05 23:21:57 +0100

    Generify chunks creation inside of the WorldGeneratorComponent

--- src/components/worldgeneratorcomponent.cpp
+++ src/components/worldgeneratorcomponent.cpp
@@ -13,11 +13,30 @@ WorldGeneratorComponent::WorldGeneratorComponent(string worldName, TerrainType t
 
     this->_perlin = siv::PerlinNoise{seed};
 
-    GameObject chunk(parent);
-    Perlin2dTerrainComponent TC(0, 0, 0, _CHUNK_X_SIZE, _CHUNK_Y_SIZE, _CHUNK_Z_SIZE, _CHUNK_NB_OF_LAYERS, _CHUNK_LAYER_SIZE_REDUCTION_FACTOR, _perlin, _OCTAVES, _FREQUENCY, _PERSISTENCE, parent);
+    int xMin = 0, xMax = 0;
+    int yMin = 0, yMax = 0; // Don't touch until vertical chunks are implemented! y should always = 0
+    int zMin = 0, zMax = 0;
+
+    int chunksCount = 0;
+    for (int x = xMin; x <= xMax; x++) {
+        for (int y = yMin; y <= yMax; y++) {
+            for (int z = zMin; z <= zMax; z++) {
+                QVector3D aabbMin(x * _CHUNK_X_SIZE, y * _CHUNK_Y_SIZE, z * _CHUNK_Z_SIZE);
+                QVector3D aabbMax((x + 1) * _CHUNK_X_SIZE, (y + 1) * _CHUNK_Y_SIZE, (z + 1) * _CHUNK_Z_SIZE);
+
+                GameObject chunk(parent);
+                chunk.GetTransform()->SetPosition(aabbMin.x(), aabbMin.y(), aabbMin.z());
+                chunk.SetFixedAABB(aabbMin, aabbMax);
+
+                Perlin2dTerrainComponent TC(0, 0, 0, _CHUNK_X_SIZE, _CHUNK_Y_SIZE, _CHUNK_Z_SIZE, _CHUNK_NB_OF_LAYERS, _CHUNK_LAYER_SIZE_REDUCTION_FACTOR, _perlin, _OCTAVES, _FREQUENCY, _PERSISTENCE, parent);
+
+                chunksCount++;
+            }
+        }
+    }
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end - start;
 
-    cout << "World generation done in " << elapsed.count() << "s (WorldGeneratorComponent)" << endl;
+    cout << "World generation done (created " << chunksCount << " chunks) in " << elapsed.count() << "s (WorldGeneratorComponent)" << endl;
 }
